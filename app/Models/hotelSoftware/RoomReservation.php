95164025843ed916bc26bd9e722b7becdb55ad48
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-12-14 16:48:08 +0100

    Fix namespaces and paths for PSR-4 compliance

--- app/Models/hotelSoftware/RoomReservation.php
+++ app/Models/hotelSoftware/RoomReservation.php
@@ -4,7 +4,6 @@ namespace App\Models\HotelSoftware;
 
 use App\Models\Payment;
 use Carbon\Carbon;
-use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 

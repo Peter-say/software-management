39ee5ad02a4a55a1c2663fb364d1128b2e4be3ee
Iriogbe Peter <iriogbepeter22@gmail.com>
2025-01-10 14:49:51 +0100

    minor changes

--- dashboard/js/plugins-init/chartjs-init.js
+++ dashboard/js/plugins-init/chartjs-init.js
@@ -322,83 +322,83 @@
 			});
 		}
 	}
-	var lineChart3 = function(){
-		//dual line chart
-		if(jQuery('#lineChart_3').length > 0 ){
-			const lineChart_3 = document.getElementById("lineChart_3").getContext('2d');
-			//generate gradient
-			const lineChart_3gradientStroke1 = lineChart_3.createLinearGradient(500, 0, 100, 0);
-			lineChart_3gradientStroke1.addColorStop(0, "rgba(249, 58, 11, 1)");
-			lineChart_3gradientStroke1.addColorStop(1, "rgba(249, 58, 11, 0.5)");
+	// var lineChart3 = function(){
+	// 	//dual line chart
+	// 	if(jQuery('#lineChart_3').length > 0 ){
+	// 		const lineChart_3 = document.getElementById("lineChart_3").getContext('2d');
+	// 		//generate gradient
+	// 		const lineChart_3gradientStroke1 = lineChart_3.createLinearGradient(500, 0, 100, 0);
+	// 		lineChart_3gradientStroke1.addColorStop(0, "rgba(249, 58, 11, 1)");
+	// 		lineChart_3gradientStroke1.addColorStop(1, "rgba(249, 58, 11, 0.5)");
 
-			const lineChart_3gradientStroke2 = lineChart_3.createLinearGradient(500, 0, 100, 0);
-			lineChart_3gradientStroke2.addColorStop(0, "rgba(255, 92, 0, 1)");
-			lineChart_3gradientStroke2.addColorStop(1, "rgba(255, 92, 0, 1)");
+	// 		const lineChart_3gradientStroke2 = lineChart_3.createLinearGradient(500, 0, 100, 0);
+	// 		lineChart_3gradientStroke2.addColorStop(0, "rgba(255, 92, 0, 1)");
+	// 		lineChart_3gradientStroke2.addColorStop(1, "rgba(255, 92, 0, 1)");
 
-			Chart.controllers.line = Chart.controllers.line.extend({
-				draw: function () {
-					draw.apply(this, arguments);
-					let nk = this.chart.chart.ctx;
-					let _stroke = nk.stroke;
-					nk.stroke = function () {
-						nk.save();
-						nk.shadowColor = 'rgba(0, 0, 0, 0)';
-						nk.shadowBlur = 10;
-						nk.shadowOffsetX = 0;
-						nk.shadowOffsetY = 10;
-						_stroke.apply(this, arguments)
-						nk.restore();
-					}
-				}
-			});
+	// 		Chart.controllers.line = Chart.controllers.line.extend({
+	// 			draw: function () {
+	// 				draw.apply(this, arguments);
+	// 				let nk = this.chart.chart.ctx;
+	// 				let _stroke = nk.stroke;
+	// 				nk.stroke = function () {
+	// 					nk.save();
+	// 					nk.shadowColor = 'rgba(0, 0, 0, 0)';
+	// 					nk.shadowBlur = 10;
+	// 					nk.shadowOffsetX = 0;
+	// 					nk.shadowOffsetY = 10;
+	// 					_stroke.apply(this, arguments)
+	// 					nk.restore();
+	// 				}
+	// 			}
+	// 		});
 				
-			lineChart_3.height = 100;
+	// 		lineChart_3.height = 100;
 
-			new Chart(lineChart_3, {
-				type: 'line',
-				data: {
-					defaultFontFamily: 'Poppins',
-					labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-					datasets: [
-						{
-							label: "My First dataset",
-							data: [25, 20, 60, 41, 66, 45, 80],
-							borderColor: lineChart_3gradientStroke1,
-							borderWidth: "2",
-							backgroundColor: 'transparent', 
-							pointBackgroundColor: 'rgba(249, 58, 11, 0.5)'
-						}, {
-							label: "My First dataset",
-							data: [5, 20, 15, 41, 35, 65, 80],
-							borderColor: lineChart_3gradientStroke2,
-							borderWidth: "2",
-							backgroundColor: 'transparent', 
-							pointBackgroundColor: 'rgba(254, 176, 25, 1)'
-						}
-					]
-				},
-				options: {
-					legend: false, 
-					scales: {
-						yAxes: [{
-							ticks: {
-								beginAtZero: true, 
-								max: 100, 
-								min: 0, 
-								stepSize: 20, 
-								padding: 10
-							}
-						}],
-						xAxes: [{ 
-							ticks: {
-								padding: 5
-							}
-						}]
-					}
-				}
-			});
-		}
-	}
+	// 		new Chart(lineChart_3, {
+	// 			type: 'line',
+	// 			data: {
+	// 				defaultFontFamily: 'Poppins',
+	// 				labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+	// 				datasets: [
+	// 					{
+	// 						label: "My First dataset",
+	// 						data: [25, 20, 60, 41, 66, 45, 80],
+	// 						borderColor: lineChart_3gradientStroke1,
+	// 						borderWidth: "2",
+	// 						backgroundColor: 'transparent', 
+	// 						pointBackgroundColor: 'rgba(249, 58, 11, 0.5)'
+	// 					}, {
+	// 						label: "My First dataset",
+	// 						data: [5, 20, 15, 41, 35, 65, 80],
+	// 						borderColor: lineChart_3gradientStroke2,
+	// 						borderWidth: "2",
+	// 						backgroundColor: 'transparent', 
+	// 						pointBackgroundColor: 'rgba(254, 176, 25, 1)'
+	// 					}
+	// 				]
+	// 			},
+	// 			options: {
+	// 				legend: false, 
+	// 				scales: {
+	// 					yAxes: [{
+	// 						ticks: {
+	// 							beginAtZero: true, 
+	// 							max: 100, 
+	// 							min: 0, 
+	// 							stepSize: 20, 
+	// 							padding: 10
+	// 						}
+	// 					}],
+	// 					xAxes: [{ 
+	// 						ticks: {
+	// 							padding: 5
+	// 						}
+	// 					}]
+	// 				}
+	// 			}
+	// 		});
+	// 	}
+	// }
 	var lineChart03 = function(){
 		//dual line chart
 		if(jQuery('#lineChart_3Kk').length > 0 ){
